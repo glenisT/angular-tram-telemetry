99a061e3e52a9ca9878074e4e0ff2e9c1c0676f9
g.tivari <g.tivari@sogetel.it>
2022-05-06 17:24:22 +0200

    Remove unwanted navigations

---
+++ src/app/app.routing.ts
@@ -0,0 +1,60 @@
+import { Routes } from "@angular/router";
+import { AdminLayoutComponent } from "./shared/components/layouts/admin-layout/admin-layout.component";
+import { AuthLayoutComponent } from "./shared/components/layouts/auth-layout/auth-layout.component";
+import { AuthGuard } from "./shared/guards/auth.guard";
+
+export const rootRouterConfig: Routes = [
+  {
+    path: "",
+    redirectTo: "/tram/analytics",
+    pathMatch: "full",
+  },
+  {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "sessions",
+        loadChildren: () =>
+          import("./views/sessions/sessions.module").then(
+            (m) => m.SessionsModule
+          ),
+        data: { title: "Session" },
+      },
+    ],
+  },
+  {
+    path: "",
+    component: AdminLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: "tram",
+        loadChildren: () =>
+          import("./views/dashboard/dashboard.module").then( (m) => m.DashboardModule),
+      },
+      {
+        path: "hvac",
+        loadChildren: () =>
+          import("./views/others/others.module").then((m) => m.OthersModule),
+        data: { title: "HVAC", breadcrumb: "HVAC" },
+      },
+      {
+        path: "passeggeri",
+        loadChildren: () =>
+          import("./views/tables/tables.module").then((m) => m.TablesModule),
+        data: { title: "Passeggeri", breadcrumb: "Passeggeri" },
+      },
+      {
+        path: "cuscinetti",
+        loadChildren: () =>
+          import("./views/tables/tables.module").then((m) => m.TablesModule),
+        data: { title: "Cuscinetti", breadcrumb: "Cuscinetti" },
+      },
+    ],
+  },
+  {
+    path: "**",
+    redirectTo: "sessions/404",
+  },
+];
