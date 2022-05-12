97f13ca056df3ed4749d95511c1abbf6ff2c243a
g.tivari <g.tivari@sogetel.it>
2022-05-12 14:41:52 +0200

    create general layout

--- src/app/views/others/app-passeggeri/app-passeggeri.component.ts
+++ src/app/views/others/app-passeggeri/app-passeggeri.component.ts
@@ -1,15 +1,339 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { matxAnimations } from "app/shared/animations/matx-animations";
+import { ThemeService } from "app/shared/services/theme.service";
+import tinyColor from "tinycolor2";
+import PerfectScrollbar from "perfect-scrollbar";
+import { WHITE_ON_BLACK_CSS_CLASS } from "@angular/cdk/a11y/high-contrast-mode/high-contrast-mode-detector";
 
 @Component({
-  selector: 'app-blank',
-  templateUrl: './app-passeggeri.component.html',
-  styleUrls: ['./app-passeggeri.component.scss']
+  selector: "app-blank",
+  templateUrl: "./app-passeggeri.component.html",
+  styleUrls: ["./app-passeggeri.component.scss"],
+  animations: matxAnimations
 })
-export class AppPasseggeriComponent implements OnInit {
+export class AppPasseggeriComponent implements OnInit, AfterViewInit {
+  trafficVsSaleOptions: any;
+  trafficVsSale: any;
+  trafficData: any;
+  saleData: any;
 
-  constructor() { }
+  sessionOptions: any;
+  sessions: any;
+  sessionsData: any;
 
+  trafficGrowthChart: any;
+  bounceRateGrowthChart: any;
+
+  dioxideChartBar: any;
+  pmvChartBar: any;
+  trafficSourcesChart: any;
+  countryTrafficStats: any[];
+  doughNutPieOptions: any;
+
+  //used for creating and customizing the gauge chart
+  gaugePpmType = "arch";
+  gaugePpmValue = 550;
+  gaugePpmMax = 1000;
+  gaugePpmLabel = "";
+  gaugePpmAppendText = "ppm";
+  gaugePpmThickness = 20;
+  gaugePpmForegroundColor = "deepSkyBlue";
+  gaugePpmBackgroundColor = "rgb(55, 55, 153)";
+  gaugePpmMarkers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugePpmSize = 200;
+
+  gaugePmvType = "arch";
+  gaugePmvValue = 1.0;
+  gaugePmvMin = 0.5;
+  gaugePmvMax = 1.5;
+  gaugePmvLabel = "";
+  gaugePmvAppendText = "pmv";
+  gaugePmvThickness = 20;
+  gaugePmvForegroundColor = "deepSkyBlue";
+  gaugePmvBackgroundColor = "rgb(55, 55, 153)";
+  gaugePmvMarkers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugePmvSize = 200;
+
+  statCardList = [
+    {
+      icon: "tram",
+      title: "Numero Veicolo",
+      amount: "2154AnZ",
+      color: "primary"
+    },
+    {
+      icon: "tag",
+      title: "Linea",
+      amount: "Linea 4",
+      color: "primary"
+    },
+    {
+      icon: "check",
+      title: "km percorsi",
+      amount: 654.21,
+      color: "primary"
+    }
+  ];
+
+  displayedColumns: string[] = ["name", "price", "available", "action"];
+
+  constructor(private themeService: ThemeService) {}
+
+  ngAfterViewInit() {}
   ngOnInit() {
+    this.themeService.onThemeChange.subscribe(activeTheme => {
+      this.initDioxideChartBar(activeTheme);
+      this.initPmvChartBar(activeTheme);
+    });
+    this.initDioxideChartBar(this.themeService.activatedTheme);
+    this.initPmvChartBar(this.themeService.activatedTheme);
+
+    //add km to km percorsi card
+    setInterval(() => {
+      for(let i = 0; i <= 0; i++)
+      {
+        this.statCardList[2].amount = +this.statCardList[2].amount + 1;
+      }
+    }, 120000);  //add 1km every 2minutes to total KM percorsi
   }
 
+  initDioxideChartBar(theme) {
+    this.dioxideChartBar = {
+      title: {
+        show:true,
+        text:"Numero passeggeri",
+        textStyle: {
+          color: "white"
+        }
+      },
+      grid: {
+        top: 56,
+        left: 56,
+        right: 25,
+        bottom: 150
+      },
+      legend: {},
+      tooltip: {
+        show: true,
+        trigger: "axis",
+
+        axisPointer: {
+          type: "cross",
+          lineStyle: {
+            opacity: 0
+          }
+        },
+        crossStyle: {
+          color: "#000"
+        }
+      },
+      series: [
+        {
+          data: [500, 550, 560, 570, 590, 500, 520, 515, 540],
+          type: "bar",
+          areaStyle: {},
+          smooth: false,
+          barStyle: {
+            width: 1,
+            color: "blue"
+          }
+        }
+      ],
+      xAxis: {
+        show: true,
+        type: "category",
+        showGrid: false,
+        boundaryGap: false,
+        data: [
+          "14:19:30",
+          "14:20:00",
+          "14:20:30",
+          "14:21:00",
+          "14:21:30",
+          "14:22:00",
+          "14:22:30",
+          "14:23:00",
+          "14:23:30"
+        ],
+        axisLabel: {
+          color: "#ccc",
+          margin: 20
+        },
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      yAxis: {
+        type: "value",
+        min: 300,
+        max: 800,
+        axisLabel: {
+          color: "#ccc",
+          margin: 20,
+          fontSize: 13,
+          fontFamily: "roboto"
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 255, 255, .1)"
+          }
+        },
+
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      color: [
+        {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "rgba(255,255,255,0.3)" // color at 0% position
+            },
+            {
+              offset: 1,
+              color: "rgba(255,255,255,0)" // color at 100% position
+            }
+          ],
+          global: false // false by default
+        }
+      ]
+    };
+  }
+
+  initPmvChartBar(theme) {
+    this.pmvChartBar = {
+      title: {
+        show:true,
+        text:"Indice decibel",
+        textStyle: {
+          color: "white"
+        }
+      },
+      grid: {
+        top: 56,
+        left: 56,
+        right: 25,
+        bottom: 150
+      },
+      legend: {},
+      tooltip: {
+        show: true,
+        trigger: "axis",
+
+        axisPointer: {
+          type: "cross",
+          lineStyle: {
+            opacity: 0
+          }
+        },
+        crossStyle: {
+          color: "#000"
+        }
+      },
+      series: [
+        {
+          data: [1, 1.1, 1.4, 1.3, 1.5, 1.3, 1.4, 1.5, 1, 1.2, 1.1, 1],
+          type: "line",
+          areaStyle: {},
+          smooth: false,
+          lineStyle: {
+            width: 2,
+            color: "#fff"
+          }
+        }
+      ],
+      xAxis: {
+        show: true,
+        type: "category",
+        showGrid: false,
+        boundaryGap: false,
+        data: [
+          "14:23:19",
+          "14:23:20",
+          "14:23:21",
+          "14:23:22",
+          "14:23:23",
+          "14:23:24",
+          "14:23:25",
+          "14:23:26",
+          "14:23:27",
+          "14:23:28",
+          "14:23:29",
+          "14:23:30",
+        ],
+        axisLabel: {
+          color: "#ccc",
+          margin: 20
+        },
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      yAxis: {
+        type: "value",
+        min: 0,
+        max: 3.0,
+        axisLabel: {
+          color: "#ccc",
+          margin: 20,
+          fontSize: 13,
+          fontFamily: "roboto"
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 255, 255, .1)"
+          }
+        },
+
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      color: [
+        {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "rgba(255,255,255,0.3)" // color at 0% position
+            },
+            {
+              offset: 1,
+              color: "rgba(255,255,255,0)" // color at 100% position
+            }
+          ],
+          global: false // false by default
+        }
+      ]
+    };
+  }
 }
