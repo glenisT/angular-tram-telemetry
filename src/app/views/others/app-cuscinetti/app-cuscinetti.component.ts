5ad446f4dfd18bd284848412f2e60774c44083be
g.tivari <g.tivari@sogetel.it>
2022-05-13 09:38:17 +0200

    Set up temperature gauges

--- src/app/views/others/app-cuscinetti/app-cuscinetti.component.ts
+++ src/app/views/others/app-cuscinetti/app-cuscinetti.component.ts
@@ -37,7 +37,7 @@ export class AppCuscinettiComponent implements OnInit, AfterViewInit {
 
   //used for creating and customizing the gauge chart
   gaugeTempType = "arch";
-  gaugeTempValue = 23.5;
+  gaugeTempValue = 23.8;
   gaugeTempMin = 23.5;
   gaugeTempMax = 24.5;
   gaugeTempLabel = "";
@@ -47,6 +47,66 @@ export class AppCuscinettiComponent implements OnInit, AfterViewInit {
   gaugeTempBackgroundColor = "rgb(55, 55, 153)";
   gaugeTempMarkers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
   gaugeTempSize = 200;
+
+  gaugeTemp2Type = "arch";
+  gaugeTemp2Value = 23.6;
+  gaugeTemp2Min = 23.5;
+  gaugeTemp2Max = 24.5;
+  gaugeTemp2Label = "";
+  gaugeTemp2AppendText = "°C";
+  gaugeTemp2Thickness = 20;
+  gaugeTemp2ForegroundColor = "deepSkyBlue";
+  gaugeTemp2BackgroundColor = "rgb(55, 55, 153)";
+  gaugeTemp2Markers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugeTemp2Size = 200;
+
+  gaugeTemp3Type = "arch";
+  gaugeTemp3Value = 24.2;
+  gaugeTemp3Min = 23.5;
+  gaugeTemp3Max = 24.5;
+  gaugeTemp3Label = "";
+  gaugeTemp3AppendText = "°C";
+  gaugeTemp3Thickness = 20;
+  gaugeTemp3ForegroundColor = "deepSkyBlue";
+  gaugeTemp3BackgroundColor = "rgb(55, 55, 153)";
+  gaugeTemp3Markers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugeTemp3Size = 200;
+
+  gaugeTemp4Type = "arch";
+  gaugeTemp4Value = 23.9;
+  gaugeTemp4Min = 23.5;
+  gaugeTemp4Max = 24.5;
+  gaugeTemp4Label = "";
+  gaugeTemp4AppendText = "°C";
+  gaugeTemp4Thickness = 20;
+  gaugeTemp4ForegroundColor = "deepSkyBlue";
+  gaugeTemp4BackgroundColor = "rgb(55, 55, 153)";
+  gaugeTemp4Markers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugeTemp4Size = 200;
+
+  gaugeTemp5Type = "arch";
+  gaugeTemp5Value = 24;
+  gaugeTemp5Min = 23.5;
+  gaugeTemp5Max = 24.5;
+  gaugeTemp5Label = "";
+  gaugeTemp5AppendText = "°C";
+  gaugeTemp5Thickness = 20;
+  gaugeTemp5ForegroundColor = "deepSkyBlue";
+  gaugeTemp5BackgroundColor = "rgb(55, 55, 153)";
+  gaugeTemp5Markers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugeTemp5Size = 200;
+
+  gaugeTemp6Type = "arch";
+  gaugeTemp6Value = 24.2;
+  gaugeTemp6Min = 23.5;
+  gaugeTemp6Max = 24.5;
+  gaugeTemp6Label = "";
+  gaugeTemp6AppendText = "°C";
+  gaugeTemp6Thickness = 20;
+  gaugeTemp6ForegroundColor = "deepSkyBlue";
+  gaugeTemp6BackgroundColor = "rgb(55, 55, 153)";
+  gaugeTemp6Markers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugeTemp6Size = 200;
 
   gaugePpmType = "arch";
   gaugePpmValue = 550;
@@ -118,6 +178,11 @@ export class AppCuscinettiComponent implements OnInit, AfterViewInit {
         this.statCardList[2].amount = +this.statCardList[2].amount + 1;
       }
     }, 120000);  //add 1km every 2minutes to total KM percorsi
+
+    if(this.gaugeTempValue > 24)
+    {
+      this.gaugeTempForegroundColor = "red";
+    }
 
   }
 }
