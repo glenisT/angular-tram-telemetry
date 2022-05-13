986ba4bf1699ce45eb77a7df02d9323edd379c53
g.tivari <g.tivari@sogetel.it>
2022-05-13 15:08:24 +0200

    Create general idea of Porte layout

--- src/app/views/others/app-porte/app-porte.component.ts
+++ src/app/views/others/app-porte/app-porte.component.ts
@@ -8,6 +8,7 @@ import { matxAnimations } from "app/shared/animations/matx-animations";
 import { ThemeService } from "app/shared/services/theme.service";
 import tinyColor from "tinycolor2";
 import PerfectScrollbar from "perfect-scrollbar";
+import { WHITE_ON_BLACK_CSS_CLASS } from "@angular/cdk/a11y/high-contrast-mode/high-contrast-mode-detector";
 
 @Component({
   selector: "app-blank",
@@ -33,6 +34,8 @@ export class AppPorteComponent implements OnInit, AfterViewInit {
   trafficSourcesChart: any;
   countryTrafficStats: any[];
   doughNutPieOptions: any;
+
+  dbChartBar: any;
 
   private oneDay = 24 * 3600 * 1000;
   private now: Date;
@@ -67,6 +70,30 @@ export class AppPorteComponent implements OnInit, AfterViewInit {
     }
   ];
 
+  //used for creating and customizing the gauge chart
+  gaugePasseggeriType = "full";
+  gaugePasseggeriValue = 54;
+  gaugePasseggeriMax = 70;
+  gaugePasseggeriLabel = "pax";
+  gaugePasseggeriAppendText = "";
+  gaugePasseggeriThickness = 20;
+  gaugePasseggeriForegroundColor = "deepSkyBlue";
+  gaugePasseggeriBackgroundColor = "rgb(55, 55, 153)";
+  gaugePasseggeriMarkers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugePasseggeriSize = 300;
+
+  gaugeDbType = "arch";
+  gaugeDbValue = 52;
+  gaugeDbMin = 0;
+  gaugeDbMax = 70;
+  gaugeDbLabel = "";
+  gaugeDbAppendText = "db";
+  gaugeDbThickness = 20;
+  gaugeDbForegroundColor = "#ff0000";
+  gaugeDbBackgroundColor = "rgb(55, 55, 153)";
+  gaugeDbMarkers = { "50": { color: "#555", type: "triangle", size: 8, label: "Goal", font: "12px arial" }};
+  gaugeDbSize = 300;
+
   displayedColumns: string[] = ["name", "price", "available", "action"];
 
   constructor(private themeService: ThemeService) {}
@@ -74,11 +101,11 @@ export class AppPorteComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {}
   ngOnInit() {
     this.themeService.onThemeChange.subscribe(activeTheme => {
-      this.initDoughNutPieOptions(activeTheme);
       this.initDailyTrafficChartBar(activeTheme);
+      this.initDbChartBar(activeTheme);
     });
     this.initDailyTrafficChartBar(this.themeService.activatedTheme);
-    this.initDoughNutPieOptions(this.themeService.activatedTheme);
+    this.initDbChartBar(this.themeService.activatedTheme);
 
      // generate some random testing data:
      this.data = [];
@@ -105,10 +132,138 @@ export class AppPorteComponent implements OnInit, AfterViewInit {
     }, 1000);
   }
 
+  initDbChartBar(theme) {
+    this.dbChartBar = {
+      title: {
+        show:true,
+        text:"Indice decibel",
+        textStyle: {
+          color: "white"
+        }
+      },
+      grid: {
+        top: 56,
+        left: 56,
+        right: 25,
+        bottom: 150
+      },
+      legend: {},
+      tooltip: {
+        show: true,
+        trigger: "axis",
+
+        axisPointer: {
+          type: "cross",
+          lineStyle: {
+            opacity: 0
+          }
+        },
+        crossStyle: {
+          color: "#000"
+        }
+      },
+      series: [
+        {
+          data: [40, 45, 50, 52, 54, 56, 61, 59, 50, 45, 47, 52],
+          type: "line",
+          areaStyle: {},
+          smooth: false,
+          lineStyle: {
+            width: 2,
+            color: "#fff"
+          }
+        }
+      ],
+      xAxis: {
+        show: true,
+        type: "category",
+        showGrid: false,
+        boundaryGap: false,
+        data: [
+          "14:18:00",
+          "14:18:30",
+          "14:19:00",
+          "14:19:30",
+          "14:20:00",
+          "14:20:30",
+          "14:21:00",
+          "14:21:30",
+          "14:22:00",
+          "14:22:30",
+          "14:23:00",
+          "14:23:30",
+        ],
+        axisLabel: {
+          color: "#ccc",
+          margin: 20
+        },
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      yAxis: {
+        type: "value",
+        min: 0,
+        max: 70,
+        axisLabel: {
+          color: "#ccc",
+          margin: 20,
+          fontSize: 13,
+          fontFamily: "roboto"
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 255, 255, .1)"
+          }
+        },
+
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        }
+      },
+      color: [
+        {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: "rgba(255,255,255,0.3)" // color at 0% position
+            },
+            {
+              offset: 1,
+              color: "rgba(255,255,255,0)" // color at 100% position
+            }
+          ],
+          global: false // false by default
+        }
+      ]
+    };
+  }
+
   initDailyTrafficChartBar(theme) {
     this.dailyTrafficChartBar = {
       title: {
-        text: 'Consumo di Corrente - Porta 1'
+        text: 'Porta 1',
+        textStyle: {
+          color: "white"
+        }
+      },
+      grid: {
+        top: 56,
+        left: 56,
+        right: 25,
+        bottom: 150
       },
       tooltip: {
         trigger: 'axis',
@@ -130,32 +285,50 @@ export class AppPorteComponent implements OnInit, AfterViewInit {
         }
       },
       yAxis: {
-        name : 'Corrente',
+        type: "value",
+        min: 0,
+        max: 1000,
+        axisLabel: {
+          color: "#ccc",
+          margin: 20,
+          fontSize: 13,
+          fontFamily: "roboto"
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255, 255, 255, .1)"
+          }
+        },
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        //name : 'Corrente',
         nameLocation: 'middle',
         nameGap: 50,
         nameTextStyle: {
           fontWeight: 'bold',
-          fontFamily: 'roboto'
+          fontFamily: 'roboto',
+          color: "white"
         },
-        type: 'value',
         boundaryGap: [0, '100%'],
         splitArea: {
           show: true
         },
-        splitLine: {
-          show: false
-        }
       },
       dataZoom: [
         {
             id: 'dataZoomX',
-            type: 'slider',
+            type: 'inside',
             xAxisIndex: [0],
             filterMode: 'filter'
         },
         {
             id: 'dataZoomY',
-            type: 'slider',
+            type: 'inside',
             yAxisIndex: [0],
             filterMode: 'empty'
         }
@@ -166,7 +339,12 @@ export class AppPorteComponent implements OnInit, AfterViewInit {
         showSymbol: false,
         hoverAnimation: false,
         data: this.data,
-        smooth:true
+        smooth:true,
+        areaStyle: {},
+        lineStyle: {
+          width: 2,
+          color: "#fff"
+         }
       }
     ]
     };
