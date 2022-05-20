8b2b0dd2e78bb099b5021eea9dab1fbeca402927
g.tivari <g.tivari@sogetel.it>
2022-05-20 18:02:56 +0200

    Change km increment usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,6 @@ export class AppComponent implements OnInit, AfterViewInit {
     for(this.i = 1; this.i <= 4; this.i++)
     {
       await this.sleep(60000); //60s per page. Make this a multiple of entire tram cycles (cycle = durationOfGiro + durationOfWaitingInStation)
-      this.dataService.statCardList[2].amount = +this.dataService.statCardList[2].amount + 1;  //add KM according to how many are run in 1 tram cycle
       this.router.navigate([this.paths[this.i]], {relativeTo: this.activeRoute});
       if(this.i == 4)
       {
@@ -54,6 +53,10 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.changePageTitle();
     // this.layout.setAppLayout()
     this.switchPages();
+
+    setInterval(() => {
+      this.dataService.statCardList[2].amount = +this.dataService.statCardList[2].amount + 1;  //add KM according to how many are run in 1 tram cycle
+    }, 5000) 
   }
   ngAfterViewInit() {
   }
